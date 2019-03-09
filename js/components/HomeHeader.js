1b9ffcbb700a59201acf3671f7fe9b0fd141b553
nicolas <nicolas.matteazzi@gmail.com>
2019-03-09 10:10:28 +0100

    Loading bar while waiting, disabled components

--- js/components/HomeHeader.js
+++ js/components/HomeHeader.js
@@ -1,24 +1,87 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TouchableOpacity, Image, Dimensions, StatusBar, TextInput, FlatList, Keyboard} from 'react-native';
+import {Platform, StyleSheet, Text, View, TouchableOpacity, Image, Dimensions, StatusBar, TextInput, FlatList, ActivityIndicator} from 'react-native';
 import TRC from 'toto-react-components';
+import { withNavigation } from 'react-navigation';
 import * as config from '../Config';
 import TrainingAPI from '../services/TrainingAPI';
 
-export default class HomeHeader extends Component {
+class HomeHeader extends Component {
 
   constructor(props) {
 
     super(props);
 
     this.state = {
-      trainingTime: null
+      trainingTime: null,
+      sessionsLoaded: false
     };
+
+    // Bindings
+    this.findOngoingSession = this.findOngoingSession.bind(this);
+    this.goToSessionExecution = this.goToSessionExecution.bind(this);
+  }
+
+  /**
+   * On component mount
+   */
+  componentDidMount() {
+
+    this.loadTodaySessions();
+
+  }
+
+  /**
+   * Loads todays sessions, if any
+   */
+  loadTodaySessions() {
+
+    new TrainingAPI().getTodaySessions().then((data) => {
+
+      this.setState({sessions: data.sessions, sessionsLoaded: true});
+
+    })
+
+  }
+
+  /**
+   * Goes to the session that is currently ongoing
+   */
+  goToSessionExecution() {
+
+    // 1. Find which session is ongoing
+    let session = this.findOngoingSession();
+
+    if (session == null) return;
+
+    this.props.navigation.navigate('SessionExecutionScreen', {sessionId: session.id});
+
+  }
+
+  /**
+   * Checks if there are ongoing sessions
+   */
+  findOngoingSession() {
+
+    if (this.state.sessions == null) return null;
+
+    for (var i = 0; i < this.state.sessions.length; i++) {
+
+      let session = this.state.sessions[i];
+
+      if (!session.completed) return session;
+    }
+
+    return null;
+
   }
 
   render() {
 
     // Components
     let leftButton, rightButton, time;
+
+    // Check if there are ongoing sessions
+    let ongoingSessions = this.findOngoingSession() != null;
 
     // Left Button
     leftButton = (
@@ -28,7 +91,23 @@ export default class HomeHeader extends Component {
     )
 
     // Time
-    if (this.state.trainingTime != null) time = (
+    if (!this.state.sessionsLoaded) time = (
+      <View style={styles.timeContainer}>
+        <Text style={styles.timeLabel}>Loading sessions</Text>
+        <View style={styles.timeTextContainer}>
+          <ActivityIndicator size="large" color={TRC.TotoTheme.theme.COLOR_THEME_LIGHT} />
+        </View>
+      </View>
+    )
+    else if (ongoingSessions) time = (
+      <View style={styles.timeContainer}>
+        <Text style={styles.timeLabel}>You're training!</Text>
+        <TouchableOpacity style={styles.timeTextContainer} onPress={this.goToSessionExecution}>
+          <Image source={require('../../img/man-training.png')} style={[styles.timeImg, {tintColor: TRC.TotoTheme.theme.COLOR_ACCENT}]} />
+        </TouchableOpacity>
+      </View>
+    )
+    else if (this.state.trainingTime != null) time = (
       <View style={styles.timeContainer}>
         <Text style={styles.timeLabel}>Today you trained</Text>
         <View style={styles.timeTextContainer}>
@@ -48,9 +127,21 @@ export default class HomeHeader extends Component {
     )
 
     // Right button
-    rightButton = (
+    if (!this.state.sessionsLoaded) rightButton = (
+      <View style={styles.timeContainer}>
+        <Text style={styles.timeLabel}></Text>
+        <View style={styles.timeTextContainer}>
+          <ActivityIndicator size="large" color={TRC.TotoTheme.theme.COLOR_THEME_LIGHT} />
+        </View>
+      </View>
+    )
+    else rightButton = (
       <View style={styles.buttonContainer}>
-        <TRC.TotoIconButton image={require('../../img/man-training.png')} size="l" label="Start training" />
+        <TRC.TotoIconButton image={require('../../img/man-training.png')}
+                            size="l"
+                            label="Start training"
+                            disabled={ongoingSessions}
+                            onPress={() => {this.props.navigation.navigate('SessionStartScreen')}}  />
       </View>
     )
 
@@ -68,6 +159,8 @@ export default class HomeHeader extends Component {
   }
 }
 
+export default withNavigation(HomeHeader);
+
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
@@ -83,6 +176,7 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     flex: 1,
     alignItems: 'center',
+    height: 100
   },
   timeLabel: {
     fontSize: 10,
